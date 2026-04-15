b1520946ef5561a38c3f8fd21d87f093c709514c
mariam <mariamgevorgyan@hotmail.com>
2026-04-15 12:50:15 -0400

    menu

---
+++ tp3_mariam_chaima/Assets/scripts/SceneQuitter.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene_quitter : MonoBehaviour
+{
+    public void quitterPartie()
+    {
+        Application.Quit();
+    }
+}
+
